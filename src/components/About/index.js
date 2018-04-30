70616d6267a37eb71247c137222c8a9ed7f73cdf
Mario Hernandez <mario.hernandez@mediacurrent.com>
2018-04-29 21:45:12 -0700

    updates bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,10 +15,21 @@ const About = () => (
       <SocialIcons />
     </div>
     <div className="about__bio">
-      <p>I live in Los Angeles area with my beautiful wife Damaris, My teenage son Diego and my little princess Valentina.</p>
-      <p>I work for <a href="https://www.mediacurrent.com">Mediacurrent</a> as a Senior Front-End Developer. I build large scale Drupal websites and I am a regular speaker at different conferences in the Los Angeles, San Diego and San Francisco areas.</p>
+      <p>
+        I live in Los Angeles area with my beautiful wife Damaris, My teenage
+        son Diego and my little princess Valentina.
+      </p>
+      <p>
+        I work for <a href="https://www.mediacurrent.com">Mediacurrent</a> as a
+        Senior Front-End Developer. I build large scale Drupal websites and I am
+        a regular speaker at different conferences in the Los Angeles, San Diego
+        and San Francisco areas.
+      </p>
 
-      <p>I conduct private and community-driven training workshops on Drupal and latest front-end trends.</p>
+      <p>
+        I conduct private and community-driven training workshops on Drupal and
+        latest front-end trends.
+      </p>
     </div>
   </header>
 );
