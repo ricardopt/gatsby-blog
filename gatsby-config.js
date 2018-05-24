1606badd0fd0238d763f01bd58d2088f88a76512
Mario Hernandez <mario.hernandez@mediacurrent.com>
2018-05-23 22:47:17 -0700

    Adds google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,20 @@ module.exports = {
     `gatsby-plugin-react-next`,
     `gatsby-plugin-catch-links`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-119804166-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
