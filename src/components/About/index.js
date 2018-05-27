143192532fa2e964cf9380e7b9f3292dadaaf03b
Mario Hernandez <mario.hernandez@mediacurrent.com>
2018-05-27 12:57:47 -0700

    Updates About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,13 +11,15 @@ const About = props => {
 	return (
 		<React.Fragment>
 			<AboutHeading id="about">About Me</AboutHeading>
+			<AboutQuote>
+				Sometimes there just aren't enough rocks.
+				<AboutCite>—Forrest Gump</AboutCite>
+			</AboutQuote>
 			<AboutWrapper>
 				<AboutHeadshot>
 					<img src={headshot} alt={`Mario Hernandez`} />
 					<AboutName>Mario Hernandez</AboutName>
 					<AboutTitle>Front-End Developer</AboutTitle>
-					<ConnectLabel>Connect with me</ConnectLabel>
-					<SocialIcons />
 				</AboutHeadshot>
 				<AboutContent>
 					<AboutBio>
@@ -28,6 +30,8 @@ const About = props => {
 
 						<p>I conduct private and community-organized training workshops on various Front-End topics.</p>
 					</AboutBio>
+					<ConnectLabel>Connect with me</ConnectLabel>
+					<SocialIcons />
 				</AboutContent>
 			</AboutWrapper>
 		</React.Fragment>
@@ -35,7 +39,6 @@ const About = props => {
 };
 
 const AboutWrapper = Styled.section`
-  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16), 0px 3px 6px rgba(0, 0, 0, 0.23);
   display: flex;
   flex-direction: column;
   max-width: 768px;
@@ -44,18 +47,87 @@ const AboutWrapper = Styled.section`
 
   @media screen and (min-width: 808px) {
     flex-direction: row;
-    margin: 0 auto 80px;
+    margin: 80px auto;
+
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      top: -60px;
+      left: calc(50% - 80px);
+      width: 160px;
+      height: 5px;
+      background: ${colors.black};
+    }
   }
 `;
 
 const AboutHeading = Styled.h3`
   width: 100%;
+  font-size: 4rem;
+  text-transform: uppercase;
+  font-weight: 700;
+  letter-spacing: 2.5px;
+  padding-bottom: 2rem;
+  padding-top: 80px;
+  border-top: 1px solid ${colors.midGray};
   text-align: center;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 0;
+    border: 0;
+  }
+`;
+
+const AboutQuote = Styled.blockquote`
+  max-width: 640px;
+  margin: 40px auto;
+  font-family: Georgia, 'Times New Roman', serif;
+  font-style: italic;
+  font-size: 3rem;
+  padding: 20px;
+  position: relative;
+  color: ${colors.darkGray};
+  text-align: center;
+
+  @media screen and (min-width: 768px) {
+    padding: 20px 0 20px 80px;
+    border-left: 4px solid ${colors.lightGray};
+    text-align: left;
+  }
+
+  &::before {
+    content: '"';
+    display: inline-block;
+    font-size: 140px;
+    position: absolute;
+    left: -10px;
+    top: -90px;
+    color: ${colors.midGray};
+    font-weight: 700;
+    font-family: cursive, serif;
+  }
+`;
+
+const AboutCite = Styled.cite`
+  display: block;
+  text-align: right;
+  font-size: 14px;
+  margin-right: 40px;
+  text-transform: uppercase;
+  font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir,
+    helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  letter-spacing: 2px;
 `;
 
 const AboutHeadshot = Styled.div`
-  flex: 0 0 300px;
-  padding-top: 20px;
+  flex: 0 0 220px;
+
+  @media screen and (min-width: 768px) {
+    margin-right: 30px;
+  }
 
   img {
     display: block;
@@ -70,19 +142,27 @@ const AboutHeadshot = Styled.div`
       box-shadow: 0px 0px 0px 12px rgba(0, 0, 0, 0.1);
     }
   }
-
-
-  ul {
-    width: 100%;
-  }
 `;
 
 const AboutContent = Styled.div`
-  padding: 30px 20px;
+  padding: 30px 0;
   position: relative;
 
-  @media screen and (min-width: 640px) {
-    padding: 60px 30px 0;
+  ul {
+    width: 100%;
+    justify-content: center;
+  }
+
+  @media screen and (min-width: 768px) {
+    padding: 0;
+
+    ul {
+      justify-content: flex-start;
+
+      li:first-child {
+        padding-left: 0;
+      }
+    }
   }
 `;
 
@@ -121,6 +201,11 @@ const ConnectLabel = Styled.p`
   font-weight: bold;
   margin-bottom: 4px;
   text-align: center;
+  padding-top: 20px;
+
+  @media screen and (min-width: 768px) {
+    text-align: left;
+  }
 `;
 
 export default About;
