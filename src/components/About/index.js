68165ff328d364785ae0ee46cf6ed934ae2fb3a5
Mario Hernandez <mario.hernandez@mediacurrent.com>
2018-06-10 14:19:45 -0700

    Updates about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,28 +10,25 @@ import { join } from 'path';
 const About = props => {
 	return (
 		<React.Fragment>
-			<AboutHeading id="about">About Me</AboutHeading>
-			{/* <AboutQuote>
-				Sometimes there just aren't enough rocks.
-				<AboutCite>—Forrest Gump</AboutCite>
-			</AboutQuote> */}
 			<AboutWrapper>
-				<AboutHeadshot>
-					<img src={headshot} alt={`Mario Hernandez`} />
-					<AboutName>Mario Hernandez</AboutName>
-					<AboutTitle>Front-End Developer</AboutTitle>
-				</AboutHeadshot>
+				<AboutHeading id="about"> About Me </AboutHeading>
 				<AboutContent>
+					<AboutHeadshot>
+						<img src={headshot} alt={`Mario Hernandez`} />
+						<SocialIcons />
+					</AboutHeadshot>
 					<AboutBio>
+						<AboutName> Mario Hernandez </AboutName>
+						<AboutTitle> Front - End Developer </AboutTitle>
 						<p>
-							I am a Front-End Developer and Speaker from Los Angeles where I live with my wife Damaris
+							I am a Front - End Developer and Speaker from Los Angeles where I live with my wife Damaris
 							and my two kids Diego and Valentina.
 						</p>
-
-						<p>I conduct private and community-organized training workshops on various Front-End topics.</p>
+						<p>
+							I conduct private and community - organized training workshops on various Front - End
+							topics.
+						</p>
 					</AboutBio>
-					<ConnectLabel>Connect with me</ConnectLabel>
-					<SocialIcons />
 				</AboutContent>
 			</AboutWrapper>
 		</React.Fragment>
@@ -39,102 +36,31 @@ const About = props => {
 };
 
 const AboutWrapper = Styled.section`
-  display: flex;
-  flex-direction: column;
   max-width: 768px;
-  margin: 0 20px 40px;
-  position: relative;
+  margin: 0 20px;
 
   @media screen and (min-width: 808px) {
-    flex-direction: row;
     margin: 20px auto 80px;
   }
 `;
 
-const AboutHeading = Styled.h3`
-  width: 100%;
-  font-size: 4rem;
-  text-transform: uppercase;
-  font-weight: 700;
-  letter-spacing: 2.5px;
-  padding-bottom: 2rem;
-  padding-top: 80px;
-  border-top: 1px solid ${colors.midGray};
-  text-align: center;
-
-  @media screen and (min-width: 768px) {
-    padding-top: 0;
-    border: 0;
-  }
-`;
-
-const AboutQuote = Styled.blockquote`
-  max-width: 640px;
-  margin: 40px auto;
-  font-family: Georgia, 'Times New Roman', serif;
-  font-style: italic;
-  font-size: 3rem;
-  padding: 20px;
-  position: relative;
-  color: ${colors.darkGray};
-  text-align: center;
-
-  @media screen and (min-width: 768px) {
-    padding: 20px 0 20px 80px;
-    border-left: 4px solid ${colors.lightGray};
-    text-align: left;
-  }
-
-  &::before {
-    content: '"';
-    display: inline-block;
-    font-size: 140px;
-    position: absolute;
-    left: -10px;
-    top: -90px;
-    color: ${colors.midGray};
-    font-weight: 700;
-    font-family: cursive, serif;
-  }
-`;
-
-const AboutCite = Styled.cite`
-  display: block;
-  text-align: right;
-  font-size: 14px;
-  margin-right: 40px;
-  text-transform: uppercase;
-  font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir,
-    helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif;
-  font-style: normal;
-  font-weight: 600;
-  letter-spacing: 2px;
-`;
-
 const AboutHeadshot = Styled.div`
-  flex: 0 0 220px;
-
-  @media screen and (min-width: 768px) {
-    margin-right: 30px;
-  }
-
   img {
     display: block;
     margin: 0 auto 20px;
-    width: 180px;
-    height: 180px;
-    border-radius: 50%;
-    box-shadow: 0px 0px 0px 8px rgba(0, 0, 0, 0.06);
-    transition: box-shadow 0.3s ease;
+    max-width: 200px;
 
-    &:hover {
-      box-shadow: 0px 0px 0px 12px rgba(0, 0, 0, 0.1);
+    @media screen and (min-width: 540px) {
+      margin-right: 30px;
     }
   }
 `;
 
 const AboutContent = Styled.div`
+  display: flex;
+  flex-direction: column;
   padding: 30px 0;
+  margin: 30px 0;
   position: relative;
 
   ul {
@@ -142,12 +68,12 @@ const AboutContent = Styled.div`
     justify-content: center;
   }
 
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: 540px) {
     padding: 0;
+    margin: 40px auto;
+    flex-direction: row;
 
     ul {
-      justify-content: flex-start;
-
       li:first-child {
         padding-left: 0;
       }
@@ -155,13 +81,34 @@ const AboutContent = Styled.div`
   }
 `;
 
+const AboutHeading = Styled.h3`
+  width: 100%;
+  font-size: 4rem;
+  text-transform: uppercase;
+  font-weight: 600;
+  letter-spacing: 2.5px;
+  padding-top: 80px;
+  border-top: 1px solid ${colors.midGray};
+  text-align: center;
+
+  @media screen and (min-width: 540px) {
+    padding-top: 0;
+    border: 0;
+    text-align: left;
+  }
+`;
+
 const AboutName = Styled.h3`
   margin-bottom: 10px;
   font-size: 1.8rem;
   font-weight: 600;
-  text-align: center;
   text-transform: uppercase;
   letter-spacing: 0.06em;
+  text-align: center;
+
+  @media screen and (min-width: 540px) {
+    text-align: left;
+  }
 `;
 
 const AboutTitle = Styled.span`
@@ -172,29 +119,12 @@ const AboutTitle = Styled.span`
   font-weight: 400;
   margin-bottom: 20px;
   text-align: center;
-  color: ${colors.midGray};
-`;
 
-const AboutBio = Styled.div`
-  position: relative;
-  z-index: 2;
-
-  @media screen and (min-width: 768px) {
-    fot-size: 1.6rem;
-  }
-`;
-
-const ConnectLabel = Styled.p`
-  text-transform: uppercase;
-  font-size: 12px;
-  font-weight: bold;
-  margin-bottom: 4px;
-  text-align: center;
-  padding-top: 20px;
-
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: 540px) {
     text-align: left;
   }
 `;
+
+const AboutBio = Styled.div``;
 
 export default About;
