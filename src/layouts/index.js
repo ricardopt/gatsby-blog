753ad68673eb306c63194856a11ab176be92c422
Mario Hernandez <mario.hernandez@mediacurrent.com>
2018-06-14 22:27:50 -0700

    Changes Prism theme to Okaidia

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,30 +6,32 @@ import Header from '../components/Header';
 import '../sass/base.scss';
 import '../sass/typography.scss';
 import '../sass/layout.scss';
-require('prismjs/themes/prism.css');
+
+// Adds prism theme
+require('prismjs/themes/prism-okaidia.css');
 
 const TemplateWrapper = ({ children }) => (
-  <React.Fragment>
-    <Helmet
-      title="Mario Hernandez"
-      meta={[
-        {
-          name: 'description',
-          content: 'Mario Hernandez is a front-end developer from Los Angeles'
-        },
-        {
-          name: 'keywords',
-          content: 'web developer, front-end web developer, public speaker'
-        }
-      ]}
-    />
-    <Header />
-    <section className="main-content">{children()}</section>
-  </React.Fragment>
+	<React.Fragment>
+		<Helmet
+			title="Mario Hernandez"
+			meta={[
+				{
+					name: 'description',
+					content: 'Mario Hernandez is a front-end developer from Los Angeles'
+				},
+				{
+					name: 'keywords',
+					content: 'web developer, front-end web developer, public speaker'
+				}
+			]}
+		/>{' '}
+		<Header />
+		<section className="main-content"> {children()} </section>{' '}
+	</React.Fragment>
 );
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func
+	children: PropTypes.func
 };
 
 export default TemplateWrapper;
